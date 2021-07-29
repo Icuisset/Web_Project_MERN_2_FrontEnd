1207f1f2e9aa97efa44950ee4dacddba8ac71f4f
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-07-29 19:21:43 +0100

    footer fully responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,10 +10,14 @@ function Footer() {
         <footer className="footer">
         <p className="footer__copyright">© 2021 Supersite, Powered by News API</p>
         <div className="footer__link-zone">
+        <div className="footer__link-zone-left">
         <Link className='footer__link' to={'/'}>Home</Link>
         <a className='footer__link' href='https://practicum.yandex.com/' target="_blank">Practicum by Yandex</a>
+        </div>
+        <div className="footer__link-zone-right">
         <a className='footer__icon' href="https://github.com/Icuisset/" target="_blank"><img src={GithubIcon} alt="GitHub icon" /></a>
         <a className='footer__icon' href="https://www.facebook.com/isawabi.london/" target="_blank"><img src={FacebookIcon} alt="Facebook icon" /></a>
+        </div>
         </div>
         </footer>
     );
