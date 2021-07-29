8f5f2ded23d9fcd0d91311428d87372d93990fad
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-07-29 17:23:30 +0100

    about fully responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,10 +11,10 @@ function About() {
             <h2 className="about__title">About Isabelle</h2>
             <p className="about__paragraph">I just launched my own web design and development business, called isaWabi.
                 I have worked for 20 years in the fashion world for major luxury brands , collaborating with talented designers and highly inspiring people . 
+                <br /><br />
                 Today, I want to continue to balance aesthetic and functionality in everything I do and put my skills and experience at the service of small businesses and solopreneurs.
                 I joined Practicum in order to learn everything about Front End Web development, including how to build dynamic web applications.</p>
         </div>
-
         </div>
     );
 }
