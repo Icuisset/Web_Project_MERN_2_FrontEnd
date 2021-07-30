e3dd63be0311d769c12e962e2def5829e29c1737
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-07-30 13:04:51 +0100

    fixed grid issue

--- src/components/SearchResultsSection/SearchResultsSection.js
+++ src/components/SearchResultsSection/SearchResultsSection.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-import './SearchResultsSection.css';
-import Cards from '../Cards/Cards';
+import "./SearchResultsSection.css";
+import Cards from "../Cards/Cards";
 
 function SearchResultsSection(props) {
-
   return (
-    <section className="searchResults">
-        <h2 className="searchResults__title">Search results</h2>
-        <Cards cards={props.cards} />
-        <button className='searchResults__showmoreButton' type='button' onClick=''>
+    <section className='searchResults'>
+      <Cards cards={props.cards} isHomePage={true} />
+      <button
+        className='searchResults__showmoreButton'
+        type='button'
+        onClick=''>
         Show more
       </button>
     </section>
