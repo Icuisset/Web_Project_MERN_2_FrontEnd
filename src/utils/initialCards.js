35c2f854aece6d90306092c72eb29e1fb9c2b264
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-08-04 10:29:58 +0100

    adjustments navbar and keyword

--- src/utils/initialCards.js
+++ src/utils/initialCards.js
@@ -1,5 +1,5 @@
 const initialCards = [{
-        "keyword": "Nature",
+        "keyword": "Naturenaturenaturenaturenature",
         "title": "Everyone Needs a Special 'Sit Spot' in Nature",
         "text": "Ever since I read Richard Louv's influential book, Last Child in the Woods, the idea of having a special sit spot has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find and be cropped",
         "date": "November 4, 2020",
