1401dfb29355440b4d772f732e861c26f189f11a
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-08-04 12:42:07 +0100

    menu icon ok in both colors

--- src/components/HeaderNavBar/HeaderNavBar.js
+++ src/components/HeaderNavBar/HeaderNavBar.js
@@ -13,7 +13,7 @@ function HeaderNavBar(props) {
       <div className='header__navigation'>
       <Link className= {`header__link header__link_textcolor_${props.textColor} header__link_focused_textcolor_${props.textColor}`} to={'/'}>Home</Link>
       <Link className= {`header__link header__link_textcolor_${props.textColor}`} to={'/saved-news'}>Saved articles</Link>
-      <div className='header__menu-icon'/>
+      <div className={`header__menu-icon header__menu-icon_textcolor_${props.textColor}`}/>
       <div className='header__signinout'>
       <Signinout isLoggedIn={props.isLoggedIn} textColor={props.textColor}></Signinout>
       </div>
