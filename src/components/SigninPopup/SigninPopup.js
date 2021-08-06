d23831f13e371dea7ab50f4d93abc22dbb7ddf56
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-08-06 09:45:53 +0100

    styling error messages ok

--- src/components/SigninPopup/SigninPopup.js
+++ src/components/SigninPopup/SigninPopup.js
@@ -6,8 +6,10 @@ import PopupWithForm from "../PopupWithForm/PopupWithForm";
 function SigninPopup(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  /* only to test CSS for now */
+  /* below states are only to test CSS for now */
   const [inputsAreValid, setInputsAreValid] = React.useState(true);
+  const [errorEmail, setErrorEmail] = React.useState(true);
+  const [errorPassword, setErrorPassword] = React.useState(false);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -30,35 +32,49 @@ function SigninPopup(props) {
       onClose={props.onClose}
       onSubmit={handleSubmit}
       alternativeLink={"Sign up"}>
-      <div className='form__input-zone'>
-        <label className='form__label' for='profileEmail'>
-          Email
-        </label>
-        <input
-          id='profileEmail'
-          className='form__input'
-          name='emailInput'
-          placeholder='Enter email'
-          type='email'
-          value={email}
-          onChange={handleEmailChange}
-          required
-        />
-        <label className='form__label' for='profilePassword'>
-          Password
-        </label>
-        <input
-          id='profilePassword'
-          className='form__input'
-          name='passwordInput'
-          placeholder='Enter password'
-          type='text'
-          value={password}
-          onChange={handlePasswordChange}
-          required
-          minLength={2}
-          maxLength={40}
-        />
+      <div className='form__inputs-zone'>
+        <div className='form__input-zone'>
+          <label className='form__label' for='profileEmail'>
+            Email
+          </label>
+          <input
+            id='profileEmail'
+            className='form__input'
+            name='emailInput'
+            placeholder='Enter email'
+            type='email'
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+          {errorEmail ? (
+            <span id='profileEmail-error' className='popup__input-error'>
+              Invalid email adress
+            </span>
+          ) : null}
+        </div>
+        <div className='form__input-zone'>
+          <label className='form__label' for='profilePassword'>
+            Password
+          </label>
+          <input
+            id='profilePassword'
+            className='form__input'
+            name='passwordInput'
+            placeholder='Enter password'
+            type='text'
+            value={password}
+            onChange={handlePasswordChange}
+            required
+            minLength={2}
+            maxLength={40}
+          />
+          {errorPassword ? (
+            <span id='profilePassword-error' className='popup__input-error'>
+              Invalid Password
+            </span>
+          ) : null}
+        </div>
       </div>
       <button
         type='submit'
