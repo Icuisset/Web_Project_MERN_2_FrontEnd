69c43c24c38329b477623910fc5eb3a21350aea2
Isabelle Cuisset <71969004+Icuisset@users.noreply.github.com>
2021-08-12 08:16:31 +0100

    showmore button showing on condition

--- src/components/SearchResultsSection/SearchResultsSection.js
+++ src/components/SearchResultsSection/SearchResultsSection.js
@@ -5,21 +5,26 @@ import Cards from "../Cards/Cards";
 import newsApi from "../../utils/NewsApi";
 
 function SearchResultsSection(props) {
-
   const handleSearchResultsClick = () => {
     console.log("Search Results button has been clicked");
   };
 
   return (
     <section className='searchResults'>
-      <Cards cards={props.cards} isHomePage={props.isHomePage} keyword={props.keyword}/>
-      <button
-        className='searchResults__showmoreButton'
-        type='button'
-        aria-label='show more'
-        onClick={handleSearchResultsClick}>
-        Show more
-      </button>
+      <Cards
+        cards={props.cards}
+        isHomePage={props.isHomePage}
+        keyword={props.keyword}
+      />
+      {props.cards.length === 3 ? (
+        <button
+          className='searchResults__showmoreButton'
+          type='button'
+          aria-label='show more'
+          onClick={handleSearchResultsClick}>
+          Show more
+        </button>
+      ) : null}
     </section>
   );
 }
